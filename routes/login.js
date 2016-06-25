3a815b1b1aaa6434cc3879bac4faa83c7af3e81d
kiku <juteng2005@gmail.com>
2016-06-25 16:52:55 +0900

    fix login and signup

--- routes/login.js
+++ routes/login.js
@@ -1,18 +1,32 @@
 /**
  * Created by mamol on 16/06/14.
+ *
+ * modified by kiku on 16/06/25
  */
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/**
+ * login画面を表示する
+ */
 router.get('/', function(req, res) {
+
+    //すでに、ログインされたユーザーを直接出退勤画面に移動する。
+    if(req.session.user){
+        res.redirect('/attendance');
+        return;
+    }
+
     res.render('login');
 });
 
+/**
+ * loginデータをチェックする。
+ */
 router.post('/',function (req,res) {
 
+    //DB設定
     var mongodb = require('mongodb');
-
     var server = new mongodb.Server('localhost',27017);
     var db = new mongodb.Db('staffManagement',server, {safe:true});
 
@@ -20,28 +34,31 @@ router.post('/',function (req,res) {
 
         if(err){
             console.log(err);
+            throw err;
         }else{
 
             var user = req.body;
 
             var col_users = db.collection('users');
 
-            col_users.find({staff_email: user.username, staff_password: user.password}).toArray(function (err,docs) {
-
-                    console.log(docs);
-
+            col_users.findOne(
+                {staff_email: user.username, staff_password: user.password},
+                function (err, item) {
                     if(err){
+                        console.log(err);
                         throw err;
-                    }else if(docs.length==0){
+                    }
+
+                    if(item){
+                        console.log('Login success');
+                        req.session.user = item;
+                        res.redirect('/attendance');
+                    }else{
                         console.log('Wrong username or password!');
                         res.redirect('/login');
-                    }else{
-                        console.log('Login success');
-                        req.session.user = docs[0];
-                        res.redirect('/attendance');
                     }
                 }
-            );
+            )
         }
     });
 });
--- routes/signup.js
+++ routes/signup.js
@@ -1,17 +1,30 @@
+/**
+ * Created by mamol on 16/06/14.
+ *
+ * modified by kiku on 16/06/25
+ */
 var express = require('express');
 var router = express.Router();
-/* GET users listing. */
+
+/**
+ * 登録画面を表示する。
+ */
 router.get('/', function(req, res) {
-
-    console.log('hello');
-
     res.render('signup');
 });
 
+/**
+ * 登録画面に登録したデータをDBに保存する。
+ */
 router.post('/',function (req,res) {
 
-    delete req.body.staff_password_confirm;
+    if(req.body.staff_password_confirm != req.body.staff_password){
+        throw {msg:'the password is not the same to the password_confirm'}
+    }else{
+        delete req.body.staff_password_confirm;
+    }
 
+    //DB設定
     var mongodb = require('mongodb');
     var server = new mongodb.Server('localhost',27017);
     var db = new mongodb.Db('staffManagement', server, {safe:true});
@@ -26,21 +39,26 @@ router.post('/',function (req,res) {
 
             var col_users = db.collection('users');
 
-            col_users.find({staff_email: user.staff_email}).toArray(function (err,docs) {
+            col_users.findOne(
+                {staff_email: user.staff_email},
+                function (err,item) {
 
-                    if(docs.length!=0){
+                    if(item){
                         console.log('this user is registered already!');
                         res.redirect('/signup');
                     }else{
-                        col_users.insert(user, function(err, result){
+                        col_users.insert(
+                            user,
+                            function(err, result){
 
-                            if(err){
-                                console.log('err in insert');
-                            }else{
-                                console.log('Sign Up Success');
-                                res.redirect('/login')
+                                if(err){
+                                    console.log('err in insert');
+                                }else{
+                                    console.log('Sign Up Success');
+                                    res.redirect('/login')
+                                }
                             }
-                        });
+                        );
                     }
                 }
             );
